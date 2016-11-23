e225a30de7da672ff3c33880c394216885e0fb42
Jony <liu.z.jonathan@gmail.com>
2016-11-23 14:55:22 -0800

    Fetching and Storing data from sensor

--- temperature.py
+++ temperature.py
@@ -1,5 +1,36 @@
 #contains code for collecting data from temperature sensor
 
-#Read json data collected from temperature sensor
+import sqlite3
+#import json
+import re
+connection = sqlite3.connect("sensor.db")
+cursor = connection.cursor()
+command = """
+CREATE TABLE data (
+entry INTEGER PRIMARY KEY,
+time DATE,
+temperature_C INTEGER,
+humidity INTEGER);"""
+cursor.execute(command)
+while True:
+    s = str(input())
+    s = s.replace("{", "")
+    s = s.replace("}", "")
+    data = s.split(",")
+    format_str = """INSERT INTO data (entry, time, temperature_C, humidity)
+    VALUES (NULL, "{time}", "{temp}", "{humidity}");"""
+    for i, d in enumerate(data):
+        if "time" in d:
+            time = d.split(":")[1].strip()
+        elif "temperature_C" in d:
+            print("Temperature: " + d.split(":")[1].strip())
+            temp = d.split(":")[1].strip()
+        elif "humidity" in d:
+            print("Humidity: " + d.split(":")[1].strip())
+            humidity = d.split(":")[1].strip()
+    command = format_str.format(time=time, temp=temp, humidity=humidity)
+    cursor.execute(command)
+    connection.commit()
+connection.close()
 
-#Export data to SQLite database
+
