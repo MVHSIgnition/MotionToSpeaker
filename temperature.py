b96763f0190e322e8a22733e257efa317439b6c7
nakulshn <nakulshn@gmail.com>
2016-11-24 16:09:20 -0800

    added graphing ability, user chooses how many times it runs, table is only created if it didn't exist before
    `

--- temperature.py
+++ temperature.py
@@ -1,12 +1,14 @@
-#contains code for collecting data from temperature sensor
-
 import sqlite3
 #import json
 import re
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from dateutil import parser
+from matplotlib import style
+style.use('fivethirtyeight')
 connection = sqlite3.connect("sensor.db")
 cursor = connection.cursor()
 
-#If the database exists, the following code won't make a new one
 command = """
 CREATE TABLE IF NOT EXISTS data (
 entry INTEGER PRIMARY KEY,
@@ -14,30 +16,36 @@ time DATE,
 temperature_C INTEGER,
 humidity INTEGER);"""
 cursor.execute(command)
-cursor.execute("SELECT * FROM data")
-for i in cursor.fetchall():
-    print(i)
-while True:
+times = int(input("How many times would you like this sensor to run?"))
+for i in range(0, 3*times):
     s = str(input())
     s = s.replace("{", "")
     s = s.replace("}", "")
-    s = s.replace("\"", "")
     data = s.split(",")
     format_str = """INSERT INTO data (entry, time, temperature_C, humidity)
     VALUES (NULL, "{time}", "{temp}", "{humidity}");"""
     for i, d in enumerate(data):
         if "time" in d:
-            time = d.split(":", 1)[1].strip()
+            time = d.split(":")[1].strip()
         elif "temperature_C" in d:
             print("Temperature: " + d.split(":")[1].strip())
-            temp = d.split(":", 1)[1].strip()
+            temp = d.split(":")[1].strip()
         elif "humidity" in d:
             print("Humidity: " + d.split(":")[1].strip())
-            humidity = d.split(":", 1)[1].strip()
+            humidity = d.split(":")[1].strip()
     command = format_str.format(time=time, temp=temp, humidity=humidity)
     cursor.execute(command)
     connection.commit()
+cursor.execute('SELECT entry, temperature_C FROM data')
+data2 = cursor.fetchall()
+
+times = []
+temps = []
+    
+for row in data2:
+    times.append(row[0])
+    temps.append(row[1])
+
+plt.plot(times,temps,'-')
+plt.show()
 connection.close()
-cursor.close()
-
-
